7127e52879aa6f5e831775bfd431e731f8a5a109
Louys Hong <louyshong1998@gmail.com>
2024-04-02 22:59:03 +0800

    Add plugin

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
-# Serverless Plugin
+# serverless-plugin-infrequent-access-logs
 
-This plugin has been generated using the `plugin` template from the [Serverless Framework](https://www.serverless.com/).
+This plugin allows you to create and configure infrequent access log groups for your lambdas. 
 
-## Implementing your plugin
+As CloudFormation [does not support changing log group classes](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-logs-loggroup.html), this plugin will instead add new infrequent access log groups to your CloudFormation stack following the pattern: 
+- `/aws/lambda/ia/${lambda}`
 
-When developing your plugin, please refer to the following sources:
+Notes: 
+- Any existing log groups managed by Serverless will remain in your CloudFormation stack.
+- When removing this plugin, any log groups created by this plugin will also be deleted from your CloudFormation stack.
+- When setting `infrequentAccessLogs: false`, any IA log groups created by this plugin will remain.
 
-- [Plugins Documentation](https://www.serverless.com/framework/docs/providers/aws/guide/plugins/)
-- [Blog - How to create serverless plugins - Part 1](https://serverless.com/blog/writing-serverless-plugins/)
-- [Blog - How to create serverless plugins - Part 2](https://serverless.com/blog/writing-serverless-plugins-2/)
+## Usage
+```
+service: sample
 
-## Sharing your plugin
+plugins:
+  - serverless-plugin-infrequent-access-logs
 
-After implementing your plugin, you might consider sharing it with a wider audience. You might do it by adding it to `Community Contributed Plugins` in official [plugins repository](https://github.com/serverless/plugins).
+provider:
+  name: aws
+
+custom:
+  infrequentAccessLogs: true # Set globally for all lambdas
+  infrequentAccessLogRetention: 30
+
+functions:
+  function1:
+  function2:
+    infrequentAccessLogs: true # Set for specific lambdas (will override the global value)
+    infrequentAccessLogRetention: 7
+```
--- index.js
+++ index.js
@@ -1,49 +1,87 @@
-'use strict';
-
-class ServerlessPlugin {
+class CloudWatchLogGroupClassPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-
-    this.commands = {
-      welcome: {
-        usage: 'Helps you start your first Serverless plugin',
-        lifecycleEvents: ['hello', 'world'],
-        options: {
-          message: {
-            usage:
-              'Specify the message you want to deploy ' +
-              '(e.g. "--message \'My Message\'" or "-m \'My Message\'")',
-            required: true,
-            shortcut: 'm',
-          },
-        },
-      },
-    };
-
+    this.provider = this.serverless.getProvider("aws");
     this.hooks = {
-      'before:welcome:hello': this.beforeWelcome.bind(this),
-      'welcome:hello': this.welcomeUser.bind(this),
-      'welcome:world': this.displayHelloMessage.bind(this),
-      'after:welcome:world': this.afterHelloWorld.bind(this),
+      "package:compileFunctions": this.beforeDeploy.bind(this),
     };
   }
 
-  beforeWelcome() {
-    this.serverless.cli.log('Hello from Serverless!');
+  isBoolean(input) {
+    if (typeof input === "boolean") {
+      return input;
+    } else {
+      throw new Error("infrequentAccessLogs must be either true or false");
+    }
   }
 
-  welcomeUser() {
-    this.serverless.cli.log('Your message:');
+  configureLogGroup(globalInfrequentAccess) {
+    const service = this.serverless.service;
+
+    if (!service.functions) {
+      return;
+    }
+
+    // Get resources specified in service file
+    service.resources = service.resources ?? {};
+    const resources = service.resources;
+    resources.Resources = resources.Resources ?? {};
+
+    Object.keys(service.functions).forEach((lambda) => {
+      const localInfrequentAccess =
+        service.functions[lambda].infrequentAccessLogs;
+
+      if (!localInfrequentAccess && !globalInfrequentAccess) {
+        // Either global or local value must exist
+        return;
+      }
+
+      // Add infrequent access log group class
+      const infrequentAccessLogGroup = {
+        Type: "AWS::Logs::LogGroup",
+        Properties: {
+          LogGroupName: `/aws/lambda/plugin/ia/${lambda}`,
+          LogGroupClass: "INFREQUENT_ACCESS",
+        },
+      };
+
+      // Decide log group class
+      const isInfrequentAccess = localInfrequentAccess
+        ? this.isBoolean(localInfrequentAccess)
+        : globalInfrequentAccess;
+
+      const lambdaLogicalId =
+        service.provider.compiledCloudFormationTemplate.Resources[
+          `${this.provider.naming.getNormalizedFunctionName(lambda)}LambdaFunction`
+        ];
+
+      const iaLogGroupLogicalId = `${lambda}PluginIALogGroup`;
+
+      if (isInfrequentAccess) {
+        // Add IA log group to service resources
+        resources.Resources[iaLogGroupLogicalId] = infrequentAccessLogGroup;
+        // Associate the lambda with the IA log group
+        lambdaLogicalId.DependsOn = [iaLogGroupLogicalId].concat(
+          lambdaLogicalId.DependsOn || []
+        );
+        lambdaLogicalId.Properties.LoggingConfig = {
+          LogGroup: iaLogGroupLogicalId
+        };
+      }
+    });
   }
 
-  displayHelloMessage() {
-    this.serverless.cli.log(`${this.options.message}`);
-  }
+  beforeDeploy() {
+    const service = this.serverless.service;
+    // Get global value if it exists
+    const globalInfrequentAccess =
+      service.custom && service.custom.infrequentAccessLogs
+        ? this.isBoolean(service.custom.infrequentAccessLogs)
+        : null;
 
-  afterHelloWorld() {
-    this.serverless.cli.log('Please come again!');
+    this.configureLogGroup(globalInfrequentAccess);
   }
 }
 
-module.exports = ServerlessPlugin;
+module.exports = CloudWatchLogGroupClassPlugin;
